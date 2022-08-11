b67143b9fb1955c4ca317811645a67c9b3b68309
Rasan Younis <rasan.younis@epfl.ch>
2022-08-12 00:12:26 +0200

    g initialisé firebase dans App.js

--- Scamna/src/App.js
+++ Scamna/src/App.js
@@ -2,7 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import TestBlock from './components/TestBlock';
 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1KOsIQvb-1dqRzefWggoYm5gertAiEhQ",
+  authDomain: "scamna-b0b94.firebaseapp.com",
+  projectId: "scamna-b0b94",
+  storageBucket: "scamna-b0b94.appspot.com",
+  messagingSenderId: "220971708276",
+  appId: "1:220971708276:web:97a5af4c5858003baa8621",
+  measurementId: "G-W8HKDF4HHW"
+}
+
+
+
+
 function App() {
+  const app = initializeApp(firebaseConfig)
   return (
     <TestBlock/>
   );
