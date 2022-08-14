af9904b32c2a41acdc82cec4ddd3d93fb73b3c67
Rasan Younis <rasan.younis@epfl.ch>
2022-08-14 17:53:20 +0200

    premier implementation de sign up qui marche sur Firebase. Styles css pour faire un peu plus bo qui sont bcp inspire de netninja

--- Scamna/src/App.js
+++ Scamna/src/App.js
@@ -1,10 +1,24 @@
+import { createContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DBClient } from './components/DBClient';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import SignUp from './components/SignUp';
 
+export const DBClientContext = createContext(null)
+
 function App() {
-  
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1KOsIQvb-1dqRzefWggoYm5gertAiEhQ",
+    authDomain: "scamna-b0b94.firebaseapp.com",
+    projectId: "scamna-b0b94",
+    storageBucket: "scamna-b0b94.appspot.com",
+    messagingSenderId: "220971708276",
+    appId: "1:220971708276:web:97a5af4c5858003baa8621",
+    measurementId: "G-W8HKDF4HHW"
+  }
+
   // // Add this in node_modules/react-dom/index.js
   // window.React1 = require('react');
 
@@ -14,11 +28,13 @@ function App() {
   // console.log(window.React1 === window.React2);
   return ( 
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <DBClientContext.Provider value={new DBClient(firebaseConfig)}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </DBClientContext.Provider>
     </Router>
   );
 }
--- Scamna/src/components/DBClient.js
+++ Scamna/src/components/DBClient.js
@@ -1,12 +1,16 @@
 import { initializeApp } from 'firebase/app'
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, QuerySnapshot, setDoc } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, QuerySnapshot, setDoc, Timestamp } from 'firebase/firestore'
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 
 export class DBClient {
     constructor(config) {
         this.app = initializeApp(config)
         this.db = getFirestore()
         this.auth = getAuth()
+    }
+
+    printTest() {
+        return "Client present"
     }
 
 
@@ -52,6 +56,24 @@ export class DBClient {
     deleteDocumentFrom(collection_name, document_id) {
         const docRef = doc(this.db, collection_name, document_id)
         return deleteDoc(docRef)
+    }
+
+    /**
+     * 
+     * @param {dictionary} userObject object containing user information
+     * @returns {Promise<DocumentReference<dictionary>>} promise resolved 
+     * with a DocumentReference pointing to the newly created document after 
+     * it has been written to the backend
+     */
+    createUser(userObject, password) {
+        const regularUserRef = collection(this.db, 'users/regular/users')
+        userObject.createdAt = Timestamp.now()
+        return createUserWithEmailAndPassword(this.auth, userObject.email, password)
+            .then(userCred => {
+                console.log(`Created user with uid ${userCred.user.uid}`)
+                return setDoc(doc(this.db, 'users/regular/users', userCred.user.uid), userObject)
+            })
+        
     }
 
 
