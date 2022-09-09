2a36f54683e9ae1c6b15f0023bbea66474202552
Rasan Younis <rasan.younis@epfl.ch>
2022-09-09 15:25:57 +0200

    css home

--- truster/src/components/Home.js
+++ truster/src/components/Home.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import InputField from "./InputField";
 import { IoMdMap } from "react-icons/io"
 import { BsCalendar3 } from "react-icons/bs"
+import { AiOutlineArrowRight } from "react-icons/ai"
 import "../styles/home.css"
+import city from "../res/city drawing.png"
 
 const Home = () => {
 
@@ -21,23 +23,37 @@ const Home = () => {
                 <h2>Moving to a new city ? Having doubts about your new home offers ?</h2>
                 <h3>Save your time and your money, get someone living nearby to visit your new place before moving in.</h3>
             </div>
-            <form className="search" onSubmit={handleSubmit}>
-                <InputField
-                    id="city"
-                    value={adress} 
-                    setValue={setAdress} 
-                    placeholder="What adress are you moving to ?"
-                    logo={<IoMdMap size={24} />}
+            <div className="content-container">
+                <img 
+                    src={city} 
+                    alt="city"
+                    width={400}
                 />
-                <InputField
-                    id="date"
-                    value={date} 
-                    setValue={setDate} 
-                    inputType="date"
-                    title="Starting from"
-                    logo={<BsCalendar3 size={24} />}
-                />
-            </form>
+                <form className="search">
+                    <InputField
+                        id="city"
+                        value={adress} 
+                        setValue={setAdress} 
+                        placeholder="What adress are you moving to ?"
+                        logo={<IoMdMap size={24} />}
+                    />
+                    <InputField
+                        id="date"
+                        value={date} 
+                        setValue={setDate} 
+                        inputType="date"
+                        title="Starting from"
+                        logo={<BsCalendar3 size={24} />}
+                    />
+                    <div className="button">
+                        <AiOutlineArrowRight 
+                            onClick={handleSubmit}
+                            size={20}
+                            strikethroughThickness={2}
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
