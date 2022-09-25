3f7d7cd4eccf34ae6b038ee7124536dc9992dea4
Menelik Nouvellon <meneliknouvellon@gmail.com>
2022-09-25 19:36:27 +0200

    pull everything

---
+++ truster/src/components/Profile/Profil.js
@@ -0,0 +1,129 @@
+import "../../styles/profile.css"
+import { useContext, useState } from "react";
+
+import { ErrorToastContext, UserContext } from "../../App";
+import { useEffect } from "react";
+import UserDetails from "./UserDetails";
+import ContactDetails from "./ContactDetails";
+import PasswordSection from "./PasswordSection";
+// import ContactDetails from "./ContactDetails";
+import UploadAndDisplayImage from "../forms/UploadAndDisplayImage";
+
+
+const Profil = ({userData, setUserData}) => {
+
+    const {user, isLoggedIn} = useContext(UserContext)
+    const displayError = useContext(ErrorToastContext)
+    const displaySuccess = useContext(ErrorToastContext)
+
+    const [userState, setUserState] = useState({
+        password: "",
+        passwordConfirmation: "",
+    })
+
+    useEffect(() => {
+        if(userData.imgUrl) {
+            setProfilePicture(userData.imgUrl)
+        }
+
+    },[userData.imgUrl])
+
+    const [profile_picture,setProfilePicture] = useState()
+    
+    const [pageNumber, setPageNumber] = useState(0)
+
+
+    const changeStyleAndCurrentPage = (number, c,o1,o2) => {
+        setPageNumber(number)
+        const curr =  document.querySelector(c)
+        const other1 = document.querySelector(o1)
+        const other2 = document.querySelector(o2)
+        curr.style.backgroundColor = '#ffcb66'
+        other1.style.backgroundColor = '#e6dca5'
+        other2.style.backgroundColor = '#e6dca5'
+    }
+
+    const submit = () => {
+        user.updatePersonalInformation(userData).then(() => {
+            console.log("Document successfully updated!");
+            displaySuccess("success","Personal information updated !")
+        })
+        .catch((error) => {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+            displayError("error","There is an error updating your profile")
+        });
+    }
+
+    const submitPassword = () => {
+        user.updateCurrentPassword(userState.password).then(console.log("updated password sucessfully"))
+    }
+
+    const changePhoto = (event) => {
+        console.log("Changing image")
+        let file = event.target.files[0]
+        console.log("image size is " + file.size)
+
+        if(file.size > 7000000){
+            console.log("File size too big")
+            displayError("error","The file size is too big. The maximum size is 7MB")
+        }else {
+            setProfilePicture(URL.createObjectURL(file));
+            console.log(profile_picture)
+            user.uploadProfilePicture(event.target.files[0], user.getUID())
+            .then(() => {
+                user.getProfilePictureURL(user.getUID()).then(url => {
+                    console.log("Changing url in database of user")
+                    let newUserData = userData
+                    newUserData["imgUrl"] = url
+                    setUserData(newUserData)
+                    submit()
+                    displaySuccess("success","Profile picture updated !")
+                })
+            })
+            
+        }
+        
+    }
+    
+
+    return ( 
+        <div className="frame">
+            <div className="topPart">
+                <div className="profilePicture">
+                    {/* {profile_picture && <UploadAndDisplayImage selectedImage={profile_picture} setSelectedImage={setProfilePicture} />} */}
+                    {profile_picture && <img src={profile_picture} width={"100%"} 
+                          height={"100%"}  alt="" />}
+                    <input
+                            className="inputfile"
+                            type="file"
+                            name="file"
+                            id="file"
+                            value={''}
+                            onChange={changePhoto}
+                            accept=".jpg, .jpeg, .png"
+                        />
+                    <label htmlFor="file">Change Image</label>
+                    
+                    
+                    
+                </div>
+
+                <div className="menu">
+                    <div className="profileSection" onClick={() => changeStyleAndCurrentPage(0,'.profileSection','.visitsSection','.postsSection')}> <p>Profile</p></div>
+                    <div className="visitsSection" onClick={() => changeStyleAndCurrentPage(1,'.visitsSection','.profileSection','.postsSection')}><p>Contact Details</p></div>
+                    <div className="postsSection" onClick={() => changeStyleAndCurrentPage(2,'.postsSection','.visitsSection','.profileSection')}><p>Password</p></div>
+                </div>
+                {/* {console.log(userData)} */}
+                {userData && pageNumber==0 && <UserDetails data={userData} user={user} setData={setUserData} onSubmit={submit} displayError={displayError} displaySuccess={displaySuccess}/>}
+                {userData && pageNumber==1 && <ContactDetails data={userData} user={user} setData={setUserData} onSubmit={submit} displayError={displayError}/>}
+                {userData && pageNumber==2 && <PasswordSection userState={userState} setUserState={setUserState} onSubmit={submitPassword} user={user}/>}
+
+                {/* {userData && pageNumber==1 && <ContactDetails data={userData} user={user} setData={setUserData} onSubmit={submit}/>} */}
+            </div>
+            
+        </div>
+     );
+}
+ 
+export default Profil;
---
+++ truster/src/components/Profile/UserDetails.js
@@ -0,0 +1,218 @@
+import Input from "../forms/Input"
+import {MdOutlineMailOutline, MdOutlineModeEditOutline} from "react-icons/md"
+import {BsPatchCheckFill, BsFillTelephoneFill} from "react-icons/bs"
+import "../../styles/userdetails.css" 
+import { deleteUser, getAuth } from "firebase/auth"
+import {useNavigate } from "react-router-dom"
+
+const UserDetails = (
+    // {gender,firstName, lastName,birthdate,email,password, passwordConfirmation,handleSubmit}
+    {data, user, setData, onSubmit, displayError, displaySuccess}
+    ) => {
+
+    const navigate = useNavigate()
+
+    const handleChange = e => {
+        let field = e.target.name
+        let newData = data
+        newData[field] = e.target.value
+        setData(newData)
+        console.log(data)
+    }
+
+    const handleAdressChange = e =>{
+        let field = e.target.name
+        let newAdress = data.adress
+        newAdress[field] = e.target.value
+        data.adress = newAdress
+        console.log(data)
+    }
+
+    const handleDeleteAccount = () => {
+        user.deleteAccount().then(() => {
+            navigate("/")
+            displaySuccess("success","Account Deleted")
+        }).catch((e) => {
+            console.log(e)
+            displayError("error","Can't delete account")
+        })
+    }
+        
+return ( 
+    <div className="profileContent">
+        
+        
+        <form>
+            
+            <div className="personal-information">
+                <h2>Personnal information</h2>
+                <div className="info">
+                    <div className="row-container">
+                        <Input 
+                            name="firstName"
+                            title="First name"
+                            value={data.firstName}
+                            className="max-width space-right"
+                            error={data.firstName.length === 0}
+                            onChange={handleChange}
+                        />
+                        <Input 
+                            name="lastName"
+                            title="Last name"
+                            value={data.lastName}
+                            className="max-width"
+                            error={data.lastName.length === 0}
+                            onChange={handleChange}
+                            
+                        />
+                    </div>
+                    <div className="row-container">
+                        <Input 
+                            name="dob"
+                            title="Date of birth"
+                            value={data.dob}
+                            inputProps={{ required:true, type:"date" }}
+                            onChange={handleChange}
+                            className="space-right"
+                        />
+                    </div>
+                    
+                    <div className="row-container gender">
+                        <label className="gender-title">Gender:</label>
+                        <div className="gender-form">
+                            <div>Male</div> 
+                            <div>Female</div> 
+                            <div>Others</div> 
+                        </div>
+                        
+                    </div>
+{/*                         
+                    
+                    <div className="row-container">
+                        <Input 
+                            name="email"
+                            title="Email"
+                            onChange={handleChange}
+                            error={data.email.length === 0}
+                            inputProps={{type:"email" }}
+                            value={data.email}
+                            className="space-right max-width"
+                            // logo=MdOutlineModeEditOutline
+                        />
+                        <div className="email-confirmed max-width">
+                            <div className="logo space-right">
+                                <MdOutlineMailOutline size={20}/>
+                            </div>
+                            {user.user.emailVerified ? "Email Confirmed" : "Email not confirmed"}
+                        </div>
+                    </div> */}
+                    
+                    
+                </div>
+                    <div className="confirmed-elements">
+                        {/* {data.emailConfimed && true} */}
+                        <div className="identity-confirmed">
+                            <div className="logo">
+                                <BsPatchCheckFill size={20}/>
+                            </div>
+                            
+                            {4 == 4 ? "Verify your identity" : "Verified"}
+                        </div>
+                        <div className="phone-confirmed">
+                            <div className="logo">
+                                <BsFillTelephoneFill size={20}/>
+                            </div>
+                        
+                            {4 == 4 ? "Add your phone number" : "Phone number : ..."}
+                        </div>
+
+                    </div>
+            
+                 </div>
+
+                 <div className="about-me">
+                    <h5>About Me</h5>
+                    <textarea name="aboutMe" onChange={handleChange}>{data.aboutMe}</textarea>
+                 </div>
+        
+            
+            
+
+
+        <div className="address-section">
+            <h2>Address</h2>
+            <div className="row-container">
+                    <Input 
+                            name="street"
+                            title="Street"
+                            value={data.adress.street}
+                            onChange={handleAdressChange}
+                            error={data.adress.street.length === 0}
+                            className="space-right max-width"
+                            // logo=MdOutlineModeEditOutline
+                    />
+
+                    <Input 
+                            name="number"
+                            title="Street Number"
+                            value={data.adress.number}
+                            error={data.adress.number.length === 0}
+                            onChange={handleAdressChange}
+                            inputProps={{type: 'number' }}
+                            className="fit-width"
+                            // logo=MdOutlineModeEditOutline
+                    />      
+            </div>
+            <div className="row-container">
+                    <Input 
+                            name="npa"
+                            title="Postal Code"
+                            value={data.adress.npa}
+                            onChange={handleAdressChange}
+                            error={data.adress.npa.length === 0}
+                            inputProps={{type: 'number' }}
+                            className="space-right fit-width"
+                            // logo=MdOutlineModeEditOutline
+                    />
+
+                    <Input 
+                            name="city"
+                            title="City"
+                            value={data.adress.city}
+                            onChange={handleAdressChange}
+                            error={data.adress.city.length === 0}
+                            className="fit-width space-right"
+                            // logo=MdOutlineModeEditOutline
+                    />      
+                    <Input 
+                            name="country"
+                            title="Country"
+                            value={data.adress.country}
+                            onChange={handleAdressChange}
+                            error={data.adress.country.length === 0}
+                            className="fit-width"
+                            // logo=MdOutlineModeEditOutline
+                    />   
+            </div>
+            
+        </div>
+        <div className="save-delete-div">
+            <div >
+                <button type="button" onClick={onSubmit} className="save-button">
+                    <h6>SAVE CHANGES</h6>
+                </button>
+            </div>
+            <div>
+                <button type="button" className="delete-button" onClick={handleDeleteAccount} >
+                    <h6>DELETE ACCOUNT</h6>
+                </button>
+            </div>
+        </div>
+        
+
+        </form>
+        
+    </div>
+ );
+}
+export default UserDetails;
