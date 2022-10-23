ff316f560ad6d7ba6ac0c42c5a44f898833b37a7
Rasan Younis <rasan.younis@epfl.ch>
2022-10-23 17:09:51 +0200

    responsive navbar

--- truster/src/components/Navbar.js
+++ truster/src/components/Navbar.js
@@ -1,13 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../App";
-import '../styles/navbar.css'
+import '../styles/responsive styles/navbar.css'
 import MenuDropdown from "./forms/MenuDropdown"
 
 const Navbar = ({setFirstPage}) => {
 
     const {user} = useContext(UserContext)
     const navigate = useNavigate()
+
+    const [activateMenu, setActivateMenu] = useState(false)
+    const toggle = (e) => {
+        e.preventDefault()
+        console.log(activateMenu)
+        setActivateMenu(!activateMenu)
+    }
 
 
     const handleLogout = e => {
@@ -22,12 +29,17 @@ const Navbar = ({setFirstPage}) => {
 
     return ( 
         <nav className="navbar">
-            <h1 onClick={handleTitle}>Truster</h1>
+            <h1 className="title" onClick={handleTitle}>Truster</h1>
+            <div className="toggle-button" onClick={toggle}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
             <div className="actions">
                 <Link to="/login">Make a visit</Link>
                 <Link to="/post">Post a visit</Link>
             </div>
-            <div className="links">
+            <div className={`links ${activateMenu ? "active" : ""}`}>
                 <Link to="/about">About</Link>
                 <Link to="/faq">FAQ</Link>
                 {!user.isLoggedIn() && <Link to="/login">Log in</Link>}
