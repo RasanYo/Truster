21d554941debb51ca7b6b4591c495c9f44e4ad51
Rasan Younis <rasan.younis@epfl.ch>
2022-10-23 17:12:03 +0200

    responsive navbar

--- truster/src/components/Navbar.js
+++ truster/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../App";
 import '../styles/responsive styles/navbar.css'
