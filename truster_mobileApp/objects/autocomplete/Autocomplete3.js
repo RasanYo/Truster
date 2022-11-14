9ee0938f698e3e7a74ea59e761f4ee6943c8efe7
Menelik Nouvellon <meneliknouvellon@gmail.com>
2022-11-14 14:18:47 +0100

    retrieve

--- truster_mobileApp/objects/autocomplete/Autocomplete3.js
+++ truster_mobileApp/objects/autocomplete/Autocomplete3.js
@@ -2,12 +2,15 @@ import { Text, View } from 'react-native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 
 
-export default function Autocomplete3(){
+export default function Autocomplete3({setAddress}){
+
+
+
     return (
           <GooglePlacesAutocomplete
-            placeholder="Type a place"
+            placeholder="Type address"
             query={{key: "AIzaSyDESnQJUVxDZRs9_3gsMV9W_arspyJFrj4"}}
-            onPress={(data, details = null) => console.log(data, details)}
+            onPress={(data, details = null) => console.log(details.terms, details)}
             onFail={error => console.log(error)}
             onNotFound={() => console.log('no results')}
             listEmptyComponent={() => (
@@ -15,6 +18,9 @@ export default function Autocomplete3(){
                     <Text>No results were found</Text>
                 </View>
                 )}
+            textInputProps={{
+                autoCorrect : false
+            }}
             styles={{
                 container : {
                     margin:0,
