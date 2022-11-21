37b8276f825a91b1e96761dbd215e5c509e50fb8
Menelik Nouvellon <meneliknouvellon@gmail.com>
2022-11-21 23:14:39 +0100

    oh yeah

--- truster_mobileApp/App.js
+++ truster_mobileApp/App.js
@@ -39,7 +39,7 @@ export default function App() {
                 title: 'Sign Up or Login'}} name="LoginMenu" component={LoginMenu}/>
                 
                 <Stack.Screen name="SignUp" component={SignUp} options={{presentation:"modal",headerShown : true,}}/>
-
+                 
                 <Stack.Screen name="PostPreview" component={PostPreview}/>
 
                 <Stack.Screen name="VisitForm" component={VisitForm}/>
