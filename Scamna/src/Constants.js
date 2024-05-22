960e0e2441eaab481bd207cb95b31ad8ca81dd4a
RasanY <85197046+RasanYo@users.noreply.github.com>
2024-05-22 12:27:15 +0200

    Delete Scamna directory

--- Scamna/src/Constants.js
+++
@@ -1,6 +0,0 @@
-export const COLLECTIONS = {
-    AVAILABLE_VISITS: "posts/notVisited/countries",
-    FINISHED_VISITS: "posts/visited/countries",
-    REGULAR_USERS: "users/regular/users",
-    ADMIN_USERS: "users/admin/users"
-}
